793c88d14b7d38613c04ba453ff46a040d9854e6
nesailormun <Keyko07@yandex.ru>
2025-02-20 16:25:07 +0300

    Init commit of Sprint 10 final project (Filmorate)

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,68 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Map<Integer, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> getAllUsers() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")){
+            throw new ValidationException("Email не соответствует требованиям.");
+        }
+        if (user.getLogin() == null || user.getLogin().contains(" ")){
+            throw new ValidationException("Имя пользователя не соответствует требованиям.");
+        }
+        if (user.getName() == null || user.getName().isBlank())
+            user.setName(user.getLogin());
+        if (user.getBirthday().isAfter(LocalDate.now()))
+            throw new ValidationException("Некорректная дата рождения.");
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user) {
+        if (user.getId() == 0) {
+            throw new ValidationException("Id должен быть указан");
+        }
+        if (!users.containsKey(user.getId())){
+            throw new NotFoundException("Пользователь с id = " + user.getId() + " не найден");
+        }
+        User updatedUsersData = users.get(user.getId());
+        if (!(user.getPassword() == null))
+            updatedUsersData.setPassword(user.getPassword());
+        if (!(user.getUsername() == null))
+            updatedUsersData.setUsername(user.getUsername());
+        if (!(user.getEmail() == null))
+            updatedUsersData.setEmail(user.getEmail());
+        return updatedUsersData;
+    }
+
+    private int getNextId() {
+        int currentMaxId = users.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(1);
+        return ++currentMaxId;
+    }
+}
+
